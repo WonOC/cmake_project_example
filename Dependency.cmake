aa34e96e29170d12b01834f4c8fc76ffcaa31356
won0 <won0@won0ui-MacBookPro.local>
2024-11-23 02:49:37 +0900

    Dependency.cmake 추가 , cmake 분리

---
+++ Dependency.cmake
@@ -0,0 +1,20 @@
+# Dependency 관련 변수 설정
+set(DEP_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
+set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
+set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
+
+# spdlog: fast logger library
+ExternalProject_Add(
+    dep-spdlog #프로젝트 대표 이름 (변수)
+    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+    GIT_TAG "v1.x"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+    TEST_COMMAND ""
+)
+# Dependency 리스트 및 라이브러리 파일 리스트 추가
+set(DEP_LIST ${DEP_LIST} dep-spdlog)
+#mac이라서 libspdlog.a로 설정 , window의 경우 spdlog.lib
+set(DEP_LIBS ${DEP_LIBS} libspdlog.a)
